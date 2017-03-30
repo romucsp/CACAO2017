d5da9e793bbeb7352c72ecdec537a2fcf21c3905
AnaisBel <anbellance@gmail.com>
2017-03-30 09:49:12 +0200

    Producteur

---
+++ CACAO2017/src/prodfeveAmerique/Producteur.java
@@ -0,0 +1,19 @@
+package prodfeveAmerique;
+
+import abstraction.fourni.v0.IProducteur;
+
+public class Producteur implements IProducteur {
+
+	@Override
+	public double quantiteMiseEnVente() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void notificationVente(double quantite) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
