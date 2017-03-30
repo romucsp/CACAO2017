63be09ce91142809ca85ff9c5981edfb432eadc0
Badkick <contact.philippeblois@gmail.com>
2017-03-30 09:49:57 +0200

    Coucou
    Coucou

--- CACAO2017/src/abstraction/intermediaireEurope/Vente.java
+++ CACAO2017/src/abstraction/intermediaireEurope/Vente.java
@@ -1,6 +1,11 @@
 package abstraction.intermediaireEurope;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
 public class Vente {
 	
 	private int numerodevente;
+	public static void main(String[] args){
+		System.out.println("Coucou");
+	}
 }
