7c88f1dad7e08310f200faae4b6cc6326ff85af4
wowalid <walidarbel77@gmail.com>
2017-03-30 11:45:19 +0200

    Creation classe Commande/Producteur

---
+++ CACAO2017/src/abstraction/distributeurseurope/Commande.java
@@ -0,0 +1,29 @@
+package abstraction.distributeurseurope;
+
+public class Commande {
+	private String Nom;
+	private int quantite;
+	
+	public Commande(String Nom, int quantite){
+		this.Nom = Nom;
+		this.quantite = quantite;
+	}
+	
+	public String getNom(){
+		return this.Nom;
+	}
+	
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	
+	
+}
---
+++ CACAO2017/src/abstraction/distributeurseurope/Producteur1.java
@@ -0,0 +1,14 @@
+package abstraction.distributeurseurope;
+import abstraction.fourni.v0.*;
+
+public class Producteur1 implements IProducteur{
+
+	public double quantiteMiseEnVente() {
+
+	}
+
+	public void notificationVente(double quantite) {
+		
+	}
+	
+}
