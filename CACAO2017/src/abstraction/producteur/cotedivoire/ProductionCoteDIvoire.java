07d06f0f38c3ee38c2b801fe35cbd72885e10f63
antoineroson <antoine.roson@gmail.com>
2017-03-31 16:31:37 +0200

    ProductionCotedivoire
    programation

--- CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
@@ -1,8 +1,11 @@
 package abstraction.producteur.cotedivoire;
 
+import abstraction.fourni.Acteur;
+import abstraction.fourni.v0.IProducteur;
+
 // modifié by fcadre
 
-public class ProductionCoteDIvoire {
+public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	private int production; 
 	private int prod_min; 
 	private int prod_max; 
@@ -12,7 +15,48 @@ public class ProductionCoteDIvoire {
 		this.prod_min=prodmin; 
 		this.prod_max=prodmax; 
 	}
-	public ProductionCoteDIvoire(){ 
-		this(1500000, 13000000, 1700000); 
+	
+	public void SetProd(int p){ 
+		this.production=p;
+	}
+	
+	public void SetProdMin(int pm){
+		this.prod_min=pm;
+	}
+	
+	public void SetProdMax(int pma){
+		this.prod_max=pma;
+	}
+	
+	public int GetProd(){
+		return this.production;
+	}
+	
+	public int GetProdMin(){
+		return this.prod_min;
+	}
+	
+	public int GetProdMax(){
+		return this.prod_max;
+	}
+	
+	public double quantiteMiseEnVente() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void notificationVente(double quantite) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
 	}
 }
