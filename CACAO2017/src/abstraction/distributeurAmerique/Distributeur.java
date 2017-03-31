ee5bbcadaf893520c05e3f26c3791c1c5a43ab51
danjud <dan.judkiewicz@gmail.com>
2017-03-31 16:17:25 +0200

    test, avancement attributs

--- CACAO2017/src/abstraction/distributeurAmerique/Distributeur.java
+++ CACAO2017/src/abstraction/distributeurAmerique/Distributeur.java
@@ -4,6 +4,10 @@ import abstraction.fourni.Acteur;
 
 public class Distributeur implements Acteur{
 	private String nom;
+	private double stock;
+	private double fonds;
+	private double demande;
+	
 	
 	public String getNom(){
 		return this.nom;
