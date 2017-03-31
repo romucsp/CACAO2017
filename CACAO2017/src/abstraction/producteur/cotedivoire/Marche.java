28971e9907ea6021914a6ef4078bbab18eab3be9
eveline boucher <eveline boucher@172.18.53.251>
2017-03-31 16:22:44 +0200

    class Marche

--- CACAO2017/src/abstraction/producteur/cotedivoire/Marche.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/Marche.java
@@ -1,7 +1,10 @@
 package abstraction.producteur.cotedivoire;
 
 import java.util.ArrayList;
+
  	
-public class Marche  {
-	
+public class Marche	{
+	public static void main(String[] args) {
+		System.out.println("coucou");
+	}
 }	
