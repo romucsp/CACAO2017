c0a342f5a004fa5252901eaebdcdbe0ce3010ba7
adripincemin <adrien.pincemin@gmail.com>
2017-03-31 16:44:07 +0200

    treso

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Tresorerie.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Tresorerie.java
@@ -1,8 +1,28 @@
 package abstraction.producteur.ameriquelatine;
 
+import abstraction.fourni.Acteur;
+import abstraction.fourni.Indicateur;
+import abstraction.fourni.Journal;
+import abstraction.fourni.Monde;
+
 public class Tresorerie {
-	private double ca;
-	private double charges;
-	private double solde;
-	private double resultat;
+	private double ca; //chiffres d'affaires de la periode
+	private double charges; // charges de production
+	private double tresorie; // argent en banque
+	private double resultat; // resultat = ca - charges
+	private String nom ;
+	
+	public Tresorerie(Monde monde, String nom){
+		this.nom = nom ;
+		
+		this.ca= ca;
+		this.resultat = this.ca - this.charges ;
+		this.charges = charges ;
+		this.tresorie = tresorie ;	
+		
+		
+
+	}
+	
+	
 }
