8e6be586d03ee0786b0e4f88624b524364d023d3
AnaisBel <anbellance@gmail.com>
2017-04-07 16:32:53 +0200

    Update Producteurs

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Producteur.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Producteur.java
@@ -3,6 +3,13 @@ package abstraction.producteur.ameriquelatine;
 import abstraction.fourni.v0.IProducteur;
 
 public class Producteur implements IProducteur {
+	private String nom;
+	private Stock stock;
+	
+	public Producteur(String nom,Stock stock){
+		this.nom=nom;
+		this.stock=stock;
+	}
 
 	@Override
 	public double quantiteMiseEnVente() {
