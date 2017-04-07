059ebdc2c470adbf19e694a0b783ee80994d1368
Ferin <kevin.ferin.3@gmail.com>
2017-04-07 17:54:50 +0200

    Evolution marché

--- CACAO2017/src/abstraction/producteur/cotedivoire/MarcheProd.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/MarcheProd.java
@@ -1,25 +1,30 @@
 package abstraction.producteur.cotedivoire;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import abstraction.fourni.Acteur;
+import abstraction.fourni.v0.IProducteur;
+import abstraction.fourni.v0.ITransformateur;
 import abstraction.fourni.v0.Producteur;
 import abstraction.fourni.v0.Transformateur;
 
-public class MarcheProd implements IMarcheProd{
 	
-	private double quantiteAchetableP1;
-	private double quantiteAchetableP2;
-	private ArrayList<Producteur> producteurs; 
-	private ArrayList<Transformateur> transformateurs; 
-	private double quantiteVoulueT1;
-	private double quantiteVoulueT2;
+	private ArrayList<IProducteur> producteurs; //liste desproducteur
+	private ArrayList<ITransformateur> transformateurs ; // liste des transformateurs
+	public double coursMin = 2500 ; // prix min du cours du cacao par tonne
+	public double coursMax = 3500 ; // prix max du cours du cacao par tonne
+	
+	private double quantiteAchetableGlob ;
+	private double quantiteVoulueGlob ;
+	private double coursActuel;
 	
 	public MarcheProd() {
-		this.producteurs= new ArrayList<Producteur>(); 
-		this.transformateurs= new ArrayList<Transformateur>();
-		this.quantiteAchetableP1=0.0;
-		this.quantiteAchetableP2=0.0;
-		this.quantiteVoulueT1=0.0;
-		this.quantiteVoulueT2=0.0;
+		this.producteurs= new ArrayList<IProducteur>();  // a modifier par IProducteur une fois que les interfaces seront créees.
+		this.transformateurs= new ArrayList<ITransformateur>(); // a modifier par ITransformateur une fois que les interfaces seront créees.
+		this.quantiteAchetableGlob=0.0;
+		this.quantiteVoulueGlob=0.0;
+		this.coursActuel=3000;
 	}
 	
 	public void addProducteur (Producteur p) {
@@ -28,10 +33,56 @@ public class MarcheProd implements IMarcheProd{
 	public void addTransformateur (Transformateur t) {
 		this.transformateurs.add(t);
 	}
-	public double quantiteAchetable() {
-		return this.quantiteAchetableP1+this.quantiteAchetableP2;
+	public double getQuantiteAchetable() {
+		return this.quantiteAchetableGlob;
 	}
-	public double quantiteVoulue() {
-		return this.quantiteVoulueT1+this.quantiteVoulueT2;
+	public double getQuantiteVoulue() {
+		return this.quantiteVoulueGlob;
+	}
+	public double getCoursActuel() {
+		return this.coursActuel;
+	}
+	public void setQuantiteAchetableGlob (double quantite) {
+		this.quantiteAchetableGlob=quantite;
+	}
+	public void setQuantiteVoulueGlob (double quantite) {
+		this.quantiteVoulueGlob=quantite;
+	}
+	public String getNom() {
+		return "Marché du cacao";
+	}
+	public void Bourse () {
+		setQuantiteAchetableGlob(0.0);
+		setQuantiteVoulueGlob(0.0);
+		
+		// La bourse va faire évoluer le coursActuel en fonction de l'offre et de la demande bornée entre coursMin et coursMax
+		if (this.quantiteAchetableGlob>=this.quantiteVoulueGlob){
+				if (this.coursActuel>this.coursMin){
+					this.coursActuel=0.99*this.coursActuel;
+				}
+		}
+		else {
+			if (this.coursActuel<this.coursMax){
+				this.coursActuel=1.01*this.coursActuel;
+			}
+		}
+	}
+	public void next() {
+		
+		
+		Bourse();
+		Map<IProducteur, Integer> m = new HashMap<IProducteur,Integer>();
+		//On creer une table de hashage qui correspond a un tableau IProd/ quantite 
+		m.put(this.producteurs.get(0), (int)this.producteurs.get(0).quantiteMiseEnVente());
+		// si on réecrit la meme ligne juste en changeant la valeur du integer ca modifie juste sa valeur donc 
+		// c'est un moyen de garder en mémoire la valeur pour chaque prod 
+		m.put(this.producteurs.get(1), (int)this.producteurs.get(0).quantiteMiseEnVente());
+		int qttTotale=0;
+		for (IProducteur p : m.keySet()){
+			qttTotale += m.get(p);
+		}
+		// Il faut faire de meme avec les transformateurs. 
+		
+		
 	}
 }
