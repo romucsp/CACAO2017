99e08e4a19de9336beb7b104d3e3219d1cb4c6ab
AnaisBel <anbellance@gmail.com>
2017-04-07 16:19:02 +0200

    Resolution conflit

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
@@ -2,38 +2,18 @@ package abstraction.producteur.ameriquelatine;
 
 public class Recolte {
 	private int surfacecultivable; //Constante : surface cultivable max
-<<<<<<< HEAD
-<<<<<<< HEAD
-	private int prodmax; // Constante production max en fonction de la surface cultivable
 	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales
 	private int Qterecoltee;
-	public Recolte(int qterecoltee, int surfacecultivable, int prodmax, double indiceRecolte){
-=======
-=======
->>>>>>> origin/master
-	
-	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales, gros pic à une période de l'annee puis diminution notable de l'indice 
 	
 	public Recolte(int qterecoltee, int surfacecultivable, double indiceRecolte){
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
 		this.surfacecultivable=surfacecultivable;
 		this.indiceRecolte=indiceRecolte;
-		Qterecoltee=(int)(prodmax*indiceRecolte);
+		Qterecoltee=(int)(surfacecultivable*indiceRecolte);
 	}
 
-<<<<<<< HEAD
 	public int getQterecoltee() {
 		return this.Qterecoltee;
-=======
-	public int Qterecoltee() {
-		return (int)(surfacecultivable*indiceRecolte);
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
+
 	}
 
 	public int getSurfacecultivable() {
