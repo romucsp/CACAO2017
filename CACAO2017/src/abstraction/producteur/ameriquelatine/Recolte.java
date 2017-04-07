fedb5e0f83b9871ddc36888ab55a5a7d2441ec3e
swerne16 <sophiewerner96@yahoo.com>
2017-04-07 16:12:58 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
@@ -2,17 +2,29 @@ package abstraction.producteur.ameriquelatine;
 
 public class Recolte {
 	private int surfacecultivable; //Constante : surface cultivable max
+<<<<<<< HEAD
 	private int prodmax; // Constante production max en fonction de la surface cultivable
 	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales
-	
+	private int Qterecoltee;
 	public Recolte(int qterecoltee, int surfacecultivable, int prodmax, double indiceRecolte){
+=======
+	
+	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales, gros pic à une période de l'annee puis diminution notable de l'indice 
+	
+	public Recolte(int qterecoltee, int surfacecultivable, double indiceRecolte){
+>>>>>>> origin/master
 		this.surfacecultivable=surfacecultivable;
-		this.prodmax=prodmax;
 		this.indiceRecolte=indiceRecolte;
+		Qterecoltee=(int)(prodmax*indiceRecolte);
 	}
 
+<<<<<<< HEAD
+	public int getQterecoltee() {
+		return this.Qterecoltee;
+=======
 	public int Qterecoltee() {
-		return (int)(prodmax*indiceRecolte);
+		return (int)(surfacecultivable*indiceRecolte);
+>>>>>>> origin/master
 	}
 
 	public int getSurfacecultivable() {
@@ -21,14 +33,6 @@ public class Recolte {
 
 	public void setSurfacecultivable(int surfacecultivable) {
 		this.surfacecultivable = surfacecultivable;
-	}
-
-	public int getProdmax() {
-		return prodmax;
-	}
-
-	public void setProdmax(int prodmax) {
-		this.prodmax = prodmax;
 	}
 
 	public double getIndiceRecolte() {
--- CACAO2017/src/abstraction/producteur/ameriquelatine/Stock.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Stock.java
@@ -5,16 +5,13 @@ public class Stock {
 	private int vente;
 	private int production;
 	private int cout;
+	private Recolte recolte;
 	
-	public Stock(int initial, int vente, int production, int cout) {
-		this.initial = initial;
+	public Stock(int initial, int vente, int production, int cout, Recolte recolte) {
+		this.initial = recolte.getQterecoltee();
 		this.vente = vente;
 		this.production = production;
 		this.cout = cout;
-	}
-	
-	public Stock() {
-		this( 0, 0, 0, 0);
 	}
 	
 	public int getInitial() {
@@ -29,4 +26,25 @@ public class Stock {
 	public int getCout() {
 		return this.cout;
 	}
+	public void setInitial ( int initial) {
+		this.initial=initial;
+	}
+	public void setVente (int vente) {
+		this.vente=vente;
+	}
+	public void setProduction (int production) {
+		this.production=production;
+	}
+	public void setCout (int cout) {
+		this.cout=cout;
+	}
+	public String toString() {
+		return "Le stock initial est de"+ this.initial+ 
+				"; "+ this.vente +"ont été vendus"+
+				"; "+ this.production + "ont été produits"+
+				"; le cout de stockage est de" + this.cout;
+	}
+	
+	
+	//modidifié et créé par swerne16
 }
