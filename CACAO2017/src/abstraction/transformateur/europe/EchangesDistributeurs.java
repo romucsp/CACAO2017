9bb83c43876e01a9019455ad298bda33c43fc2e5
Guitouune <guillaume.halzuet7@gmail.com>
2017-04-07 18:12:50 +0200

    modif prixmin dans echangedistributeurs
    modif et ajout de variables

--- CACAO2017/src/abstraction/transformateur/europe/EchangesDistributeurs.java
+++ CACAO2017/src/abstraction/transformateur/europe/EchangesDistributeurs.java
@@ -2,17 +2,28 @@ package abstraction.transformateur.europe;
 
 public class EchangesDistributeurs {
 
-	private double prixmin;
+	private double prixmin;     //prix minimum fixé par nous pour ne pas vendre a perte
+	private double prixachat;	//prix souhaité par le distributeur 
+	private double a;			//facteur a déterminé pour la régression linéaire du prixmin en fonction des stocks restant, du prix du cacao etc.
+	private double b; 			// idem
 	
 	public EchangesDistributeurs(double prixmin){
 		this.prixmin=prixmin;
 	}
 	
 	public double getPrixmin(){
-		return this.prixmin;
+		if (this.prixmin>prixachat){
+			return 10000;
+		}
+		else{
+			this.prixmin=a*this.prixmin+b;
+			return this.prixmin;
+		}
 	}
 	
 	public void setPrixmin(double prixmin){
 		this.prixmin=prixmin;
 	}
+	
+	
 }
