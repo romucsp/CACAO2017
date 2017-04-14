6667625f2f6cb35b61fcf0fb3c7747c183cc08ea
Badkick <contact.philippeblois@gmail.com>
2017-04-14 16:53:10 +0200

    Classe Intermédiaire Producteur
    Création instances classe intermédiaire producteur + modification classe
    stock

--- CACAO2017/src/abstraction/transformateur/europe/EchangesProducteurs.java
+++ CACAO2017/src/abstraction/transformateur/europe/EchangesProducteurs.java
@@ -1,5 +1,29 @@
 package abstraction.transformateur.europe;
 
 public class EchangesProducteurs {
-
+	
+	private double quantiteSouhaitee;
+	private Stock s;
+	
+	public EchangesProducteurs(double quantiteSouhaitee,Stock s){
+		this.quantiteSouhaitee = quantiteSouhaitee;
+		this.s=s;
+	}
+	
+	public double getQuantiteSouhaitee(){
+		return this.quantiteSouhaitee;
+	}
+	
+	public Stock getStock(){
+		return this.s;
+	}
+	
+	public double getQuantiteMax(){
+		double stockCacao=this.s.getstockcacao();
+		double stockMax=this.s.getStockMax();
+		return stockMax-stockCacao;
+		
+	}
+	
+	
 }
