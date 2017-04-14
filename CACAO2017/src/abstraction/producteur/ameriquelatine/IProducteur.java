c5e469c2b3b3c02d3008ddd527807f37418cc48a
adripincemin <adrien.pincemin@gmail.com>
2017-04-14 15:15:47 +0200

    IProducteur

--- CACAO2017/src/abstraction/producteur/ameriquelatine/IProducteur.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/IProducteur.java
@@ -1,6 +1,7 @@
 package abstraction.producteur.ameriquelatine;
 
 public interface IProducteur {
+	//Adrien
 	public double quantiteMiseEnvente() ;
 	public void notificationVente(double quantite, double coursActuel); // cours actuel, pris à la tonne
 	
