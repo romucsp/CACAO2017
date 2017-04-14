fb3f323707b0cf899cbe3513e06fb6e4d82ca6c9
wowalid <walidarbel77@gmail.com>
2017-04-14 14:47:34 +0200

    Modification de marché : utilisation de listes

--- CACAO2017/src/abstraction/distributeur/europe/Marche.java
+++ CACAO2017/src/abstraction/distributeur/europe/Marche.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import abstraction.fourni.Acteur;
 
-/*public class Marche {
+public class Marche {
 	
 	private ArrayList<IDistributeur> distributeur;
 	private ArrayList<ITransformateur> transformateur;
@@ -22,6 +22,31 @@ import abstraction.fourni.Acteur;
 		this.transformateur=transformateur;
 	}
 	
+	public boolean testFourchettePrix(boolean[] test_prix){
+		for (int i=0; i<test_prix.length; i++){
+			if ((test_prix[i]) == true){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int indiceMaximum(){
+		int indice_max = 0;
+		for (int i=1; i<this.distributeur.size(); i++){
+			indice_max = ((this.distributeur.get(i).getPrixMax()>this.distributeur.get(indice_max).getPrixMax()) ? i : indice_max);
+		}
+		return indice_max;
+	}
+	
+	public int indiceMinimum(){
+		int indice_min = 0;
+		for (int i=1; i<this.distributeur.size(); i++){
+			indice_min = ((this.distributeur.get(i).getPrixMax()<this.distributeur.get(indice_min).getPrixMax()) ? i : indice_min);
+		}
+		return indice_min;		
+	}
+	
 	public void Echanges(){
 		
 	
@@ -29,50 +54,46 @@ import abstraction.fourni.Acteur;
 		
 		
 		// On vérifie si les prix sont dans la fourchette autorisée
-		
-		boolean test1= (transformateur.get(1).getPrixMin()>=prixVenteMin) &&	(transformateur.get(1).getPrixMin()<=prixVenteMax);	
-		boolean test2= (transformateur.get(2).getPrixMin()>=prixVenteMin) &&	(transformateur.get(2).getPrixMin()<=prixVenteMax);
-		
-		boolean test3= (distributeur.get(1).getPrixMax()>=prixAchatMin) &&  (distributeur.get(1).getPrixMax()<=prixAchatMax);
-		boolean test4= (distributeur.get(2).getPrixMax()>=prixAchatMin) &&  (distributeur.get(2).getPrixMax()<=prixAchatMax);
+		boolean[] test_t = new boolean[this.transformateur.size()];
+		boolean[] test_d = new boolean[this.transformateur.size()];
+		for (int i=0; i<this.transformateur.size(); i++){
+			test_t[i] = (transformateur.get(i).getPrixMin()>=prixVenteMin) &&	(transformateur.get(i).getPrixMin()<=prixVenteMax);
+			test_d[i] = (distributeur.get(i).getPrixMax()>=prixAchatMin) &&  (distributeur.get(i).getPrixMax()<=prixAchatMax);
+		}
+
 		
 		// On vérifie qu'une transaction soit possible
 		
-		 boolean testPrix= Math.max(distributeur.get(1).getPrixMax(), distributeur.get(2).getPrixMax()) >= Math.max(transformateur.get(1).getPrixMin(), transformateur.get(2).getPrixMin());
+		 boolean testPrix= true;
+				
+		 for (int i=0; i<this.transformateur.size(); i++){
+			 if ( Math.max(distributeur.get(1).getPrixMax(), distributeur.get(2).getPrixMax()) < Math.max(transformateur.get(1).getPrixMin(), transformateur.get(2).getPrixMin())){
+				 testPrix = false;
+			 }
+		 }
 		 
-		 
-		
-		while ((test1 || test2 || test3 || test4) && testPrix){
-			
-			
 			// Initialisation des deux variables définissant l'indice du distributeur et transformateur prioritaires
 			
 			int prioDistri=0;
 			int prioTransfo=0;
 			
+		 	if ((testFourchettePrix(test_t)||testFourchettePrix(test_d)) && testPrix){
+			
 			// Recherche de minimum & maximum
 			
-			for (int i=0;i<2;i++){
-				if (distributeur.get(i).getPrixMax()>=distributeur.get(prioDistri).getPrixMax()){
-					prioDistri=i;
-				}
-				if (transformateur.get(i).getPrixMin()<=transformateur.get(i).getPrixMin()){
-					prioTransfo=i;
-				}
+			prioDistri = this.indiceMaximum();
+			prioTransfo = this.indiceMinimum();
 			}
 			
 			// Définition du prix
 			
-			double prix = (distributeur.get(prioDistri).getPrixMax()+transformateur.get(i).getPrixMin())/2;
+			double prix = (distributeur.get(prioDistri).getPrixMax()+transformateur.get(prioTransfo).getPrixMin())/2;
 			
 			int autreDistri=Math.abs(prioDistri-1);
 			int autreTransfo=Math.abs(prioTransfo-1);
 			
 			
 			// Actualisation des prix d'achat et de vente
-			
-			
-	
 			
 			distributeur.get(prioDistri).notif(new Vente(prix, unite));
 			distributeur.get(autreDistri).notif(new Vente(prix, 0));
@@ -82,8 +103,5 @@ import abstraction.fourni.Acteur;
 			
 		}
 		
-	}
-	
-}	
-*/
+}
 
