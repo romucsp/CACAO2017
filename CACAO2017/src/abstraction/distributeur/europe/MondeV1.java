9a866aee25fc86cf7f00aa19a8f4d096cf226d32
NumaB <numa.beguerie@gmail.com>
2017-04-14 16:50:52 +0200

    Update MondeV1

--- CACAO2017/src/abstraction/distributeur/europe/MondeV1.java
+++ CACAO2017/src/abstraction/distributeur/europe/MondeV1.java
@@ -9,11 +9,21 @@ public class MondeV1 extends Monde {
 	 *Création de l'instance Monde pour la version 1
 	 */
 	
-	private ArrayList<Distributeur> distributeurs;
+	private ArrayList<IDistributeur> distributeurs;
 	private ArrayList<ITransformateur> transformateurs;
 	private ArrayList<IProducteur> producteurs;
 	
-	Distributeur dEuropeen = new Distributeur(new Vente(70000000,1), 13, 13);
+	public void peupler(){
+		
+		Distributeur dEuropeen = new Distributeur(new Vente(70000000,1), 13, 13);
+		
+		distributeurs.add(dEuropeen);
+		
+		Marche marche2 = new Marche(this.distributeurs, this.transformateurs);
+		
+		
+	}
+	
 	
 
 }
