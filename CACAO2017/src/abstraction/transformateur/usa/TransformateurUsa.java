ae031f8ea6fbb275690c6ba44c80c0ed4a7a3319
BraveMole <bsouchu@gmail.com>
2017-04-14 15:02:49 +0200

    ff

--- CACAO2017/src/abstraction/transformateur/usa/TransformateurUsa.java
+++ CACAO2017/src/abstraction/transformateur/usa/TransformateurUsa.java
@@ -1,30 +1,28 @@
 package abstraction.transformateur.usa;
+import abstraction.fourni.Acteur;
 import abstraction.transformateur.usa.interfacemarche.*;
 import abstraction.producteur.cotedivoire.*;
 //Souchu
-public class TransformateurUsa implements transformateur{
-	private StockProduitsFinis finis = new StockProduitsFinis(0);
+public class TransformateurUsa implements transformateur,Acteur{
+	private StockProduitsFinis finis = new StockProduitsFinis(1000000);
 	private StockMatPremiere premiere;
+	private TransfoChocolat Transfo=new TransfoChocolat(premiere,finis);
 	private Tresorie tresorie=new Tresorie();
 	private int unitéventechocolat=100;
 	private double bornesmax=8;
 	private double bornesmin=4;
 	
 	public void next(){
-		achatmatpremiere();
 		produirechocolat();
+		achetermatièrepremière();
 	}
-	
-	public void Onestlivréenmatièrepremière(){
-		
-	}
-	
-	public void achatmatpremiere(){
+	public void achetermatièrepremière(){
 		
 	}
 	
 	public void produirechocolat(){
-		
+		double StockSouhaite =1000000;
+		Transfo.produireChoco(StockSouhaite-finis.getStockChocolat());
 	}
 	
 	public double getprixMin(){
@@ -42,14 +40,17 @@ public class TransformateurUsa implements transformateur{
 	@Override
 	public void notif(double prix, double quantité) {
 		this.finis.enleverChoco(quantité);
-		this.tresorie.setCompteCourant(this.tresorie.getCompteCourant()+quantité*prix);
-		
+		this.tresorie.setCompteCourant(this.tresorie.getCompteCourant()+quantité*prix);	
 	}
 	
 	public double QteSouhaite(){
 		double q= 10000000;
 		return q-this.premiere.getCacao();
 		
+	}
+	@Override
+	public String getNom() {
+		return "Transfo Usa";
 	}
 	
 	public void notificationAchat(double vendu, double prix){
