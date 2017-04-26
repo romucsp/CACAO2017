a3af6c0f9acfc6f91b2c620780fab477129039d8
Guitouune <guillaume.halzuet7@gmail.com>
2017-04-26 16:09:49 +0200

    completion de transformateurs

--- CACAO2017/src/abstraction/transformateur/europe/Transformateur.java
+++ CACAO2017/src/abstraction/transformateur/europe/Transformateur.java
@@ -5,6 +5,27 @@ public class Transformateur implements transformateur  {
 
 	private String nom;
 	private Stock s;
+	private Tresorerie compte;
+	
+	public double getprixMin() {
+		double a = 1000*Math.random();
+		double prixmin = 4000;
+		double stockchocolat=this.s.getstockchocolat();
+		if (stockchocolat<100){
+			return 10000;
+		}
+		else{
+			return prixmin+a;
+		}
+		// TODO Auto-generated method stub
+	}
+	
+	public void notif(double prix, double quantité) {
+		// TODO Auto-generated method stub
+		this.s.setstockchocolat(this.s.getstockchocolat()-quantité);
+		double chiffre_daffaire=prix*quantité;
+		this.compte.ajoutChiffredaffaire(chiffre_daffaire);
+	}
 	
 	public int hashCode() {
 		return this.getNom().hashCode();
