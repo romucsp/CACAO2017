cf6109a48d0e01fefa128f92ea4749cc27c4940e
Badkick <contact.philippeblois@gmail.com>
2017-04-26 15:13:13 +0200

    Interface

--- CACAO2017/src/abstraction/transformateur/europe/Vente.java
+++ CACAO2017/src/abstraction/transformateur/europe/Vente.java
@@ -1,11 +1,33 @@
-package abstraction.transformateur.europe;
+// package abstraction.transformateur.europe;
 
-import com.sun.java_cup.internal.runtime.Scanner;
+//import com.sun.java_cup.internal.runtime.Scanner;
 
-public class Vente {
+//public class Vente {
 	
-	private int numerodevente;
-	private int prix;
-	private String Theo;
+//	private int numerodevente;
+//	private int prix;
+// private String Theo; 
 	
-}
+//}
+ /*********************************************** EXEMPLE D'INTERFACES
+*public interface NoiseMaker {
+*   String noise = "Making Noise"; // interface variables are public static final by default
+*
+*  String makeNoise(); //interface methods are public abstract by default
+*}
+*
+*public interface FoodEater {
+*    void eat(Food food);
+* }
+*public class Cat implements NoiseMaker, FoodEater { 
+*@Override
+* public String makeNoise() {
+*      return "meow";
+*   }
+*
+*   @Override
+*    public void eat(Food food) {
+*       System.out.println("meows appreciatively");
+*   }
+*}
+**************************************************************************///////
