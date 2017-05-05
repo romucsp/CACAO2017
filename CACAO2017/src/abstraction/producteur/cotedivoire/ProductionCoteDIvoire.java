53b77ee1dba70e02220be8403d0c9644e1dc271f
Ferin <kevin.ferin.3@gmail.com>
2017-05-05 16:53:37 +0200

    modif variable production

--- CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
@@ -3,13 +3,12 @@ package abstraction.producteur.cotedivoire;
 import abstraction.fourni.Acteur;
 import abstraction.producteur.ameriquelatine.IProducteur;
 
-import java.util.ArrayList; 
 
 // by fcadre, comments by antoineroson
 
 public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	public static final int  productionmoyenne = 1650000; // Production moyenne de la cote d'ivoire en tonnes
-	private ArrayList<Integer>  productions; //Liste des productions par périodes
+	private int  productions; //Liste des productions par périodes
 	private Stock stock;          // Represente notre stock 
 	private Treso tresorerie;     // Représente notre trésorerie
 	
@@ -19,13 +18,13 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	}
 	
 	//Constructeur Production cote d'ivoire
-	public ProductionCoteDIvoire(ArrayList<Integer> prods, Stock stock, Treso treso){ 
+	public ProductionCoteDIvoire(int prods, Stock stock, Treso treso){ 
 		this.productions = prods; 
 		this.stock=stock;
 		this.tresorerie = treso; 
 	}
 	public ProductionCoteDIvoire() {
-		this.productions = new ArrayList<Integer>();
+		this.productions = 0;
 		this.stock= new Stock(0);
 		this.tresorerie= new Treso();
 	}
@@ -36,13 +35,13 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	}
 	
 	//Accesseur liste de production
-	public ArrayList<Integer> getProductions(){
+	public int getProductions(){
 		return this.productions; 
 	}
 	
 	//Accesseur quantité produite
 	public int getQuantiteProd(){ 
-		return this.getProductions().get(this.productions.size()-1);   
+		return this.getProductions();   
 		// Récupére la dernière production sur la période
 	}
 
@@ -53,7 +52,7 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 		double prod_min = this.getProductionmoyenne() - (double)(this.getProductionmoyenne()*variation); 
 		double prod_max = this.getProductionmoyenne() + (double)(this.getProductionmoyenne()*variation);
 		double prod = prod_min + (double)Math.random()*(prod_max - prod_min); // Production random entre prod_min et prod_max
-		productions.add((int)prod); // ajout dans la liste de production
+		productions=(int)prod; // ajout dans la liste de production
 		this.stock.addStock(prod);
 	}
 	
