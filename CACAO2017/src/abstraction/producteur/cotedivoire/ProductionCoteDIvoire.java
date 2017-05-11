810fb3661668cf0246762386ba387bd8dbb24002
antoineroson <antoine.roson@gmail.com>
2017-05-11 16:44:18 +0200

    Add Indicateur
    stock+treso

--- CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
@@ -9,11 +9,13 @@ import abstraction.producteur.ameriquelatine.IProducteur;
 // by fcadre, comments by antoineroson
 
 public class ProductionCoteDIvoire implements Acteur, IProducteur{
-	public static final int  productionmoyenne = 1650000; // Production moyenne de la cote d'ivoire en tonnes
+	public static final int  productionmoyenne = 165; // Production moyenne de la cote d'ivoire en tonnes
 	private int  production; //Liste des productions par périodes
 	private Stock stock;          // Represente notre stock 
 	private Treso tresorerie;     // Représente notre trésorerie
 	private Indicateur productionIndicateur;
+	private Indicateur stockIndicateur;
+	private Indicateur tresoIndicateur;
 	//Cf marché
 	public int hashCode() {
 		return this.getNom().hashCode();
@@ -31,6 +33,10 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 		this.tresorerie= new Treso();
 		this.productionIndicateur=new Indicateur("6_PROD_COT_production",this,0.0);
 		Monde.LE_MONDE.ajouterIndicateur( this.productionIndicateur );
+		this.stockIndicateur = new Indicateur("6_PROD_COT_stock",this,0.0);
+		Monde.LE_MONDE.ajouterIndicateur(this.stockIndicateur);
+		this.tresoIndicateur = new Indicateur("6_PROD_COT_treso",this,0.0);
+		Monde.LE_MONDE.ajouterIndicateur(this.tresoIndicateur);
 	}
 	
 	//Accesseur Production moyenne
@@ -56,9 +62,9 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 		double prod_min = this.getProductionmoyenne() - (double)(this.getProductionmoyenne()*variation); 
 		double prod_max = this.getProductionmoyenne() + (double)(this.getProductionmoyenne()*variation);
 		double prod = prod_min + (double)Math.random()*(prod_max - prod_min); // Production random entre prod_min et prod_max
-		production=(int)prod; // ajout dans la liste de production
-		this.stock.addStock(prod);
-		this.productionIndicateur.setValeur(this, prod);
+		this.production=(int)prod; // ajout dans la liste de production
+		this.stock.addStock((int)prod);
+		this.productionIndicateur.setValeur(this, (int)prod);
 	}
 	
 	//Accesseur Nom
@@ -72,13 +78,17 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 
 
 	public void notificationVente(double quantite, double coursActuel) {	// grace a la notification de vente on met a jour // 
-		this.tresorerie.addCa(quantite*coursActuel);                       // notre tresorerie et notre stock //
-		this.stock.addStock(-quantite);	
+		this.tresorerie.addCa(quantite*coursActuel);   // notre tresorerie et notre stock //
+		
+		this.stock.addStock(-quantite);
+		
 	}
 	
 	//NEXT "Centre du programme -> Passage à la période suivante" 
 	
 	public void next() {
 		this.variationProduction();	
+		this.stockIndicateur.setValeur(this,this.stock.getStock());
+		this.tresoIndicateur.setValeur(this,this.tresorerie.getCa());
 	}
 }
